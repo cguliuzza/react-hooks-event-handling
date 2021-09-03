1fc9afac95c1e7c2c2c8841134879f2031e683ad
christinaguliuzza <christina.guliuzza@gmail.com>
2021-09-03 10:24:12 -0700

    Walked through example

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+// function Tickler() {
+//   return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
+// }
+
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
